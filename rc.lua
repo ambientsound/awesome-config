5e2e0beba9b3eb81948ff9f7429e8330f588c401
Kim Tore Jensen <kim.tore@freecode.no>
2011-12-08 13:48:33 +0100

    Initial commit

---
+++ rc.lua
@@ -0,0 +1,260 @@
+----- ambientsound's awesome config -----
+
+-- Standard awesome library
+require("awful")
+require("awful.autofocus")
+require("awful.rules")
+-- Theme handling library
+require("beautiful")
+-- Notification library
+require("naughty")
+-- Vicious widgets
+require("vicious")
+
+-- Load our beauty
+beautiful.init(awful.util.getdir("config") .. "/themes/ambient/theme.lua")
+
+-- {{{ Variable definitions
+-- Themes define colours, icons, and wallpapers
+--beautiful.init("/usr/share/awesome/themes/zenburn/theme.lua")
+
+-- This is used later as the default terminal and editor to run.
+terminal = "urxvt"
+editor = os.getenv("EDITOR") or "vim"
+editor_cmd = terminal .. " -e " .. editor
+
+-- Default modkey.
+-- Usually, Mod4 is the key with a logo between Control and Alt.
+-- If you do not like this or do not have such a key,
+-- I suggest you to remap Mod4 to another key using xmodmap or other tools.
+-- However, you can use another modifier like Mod1, but it may interact with others.
+altkey = "Mod1"
+modkey = "Mod4"
+
+-- Table of layouts to cover with awful.layout.inc, order matters.
+layouts =
+{
+    awful.layout.suit.tile,
+    awful.layout.suit.tile.left,
+    awful.layout.suit.tile.bottom,
+    awful.layout.suit.tile.top,
+    awful.layout.suit.max
+}
+-- }}}
+
+
+-- {{{ Tags
+-- Define a tag table which hold all screen tags.
+tags = {}
+for s = 1, screen.count() do
+    -- Each screen has its own tag table.
+    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[5])
+end
+-- }}}
+
+
+-- {{{ Wibox
+-- Create a textclock widget
+--mytextclock = awful.widget.textclock({ align = "right" })
+
+-- Create a systray
+--mysystray = widget({ type = "systray" })
+
+-- Create a wibox for each screen and add it
+--mywibox = {}
+--for s = 1, screen.count() do
+    --mywibox[s].widgets = {
+        --mytextclock,
+        --s == 1 and mysystray or nil,
+        --layout = awful.widget.layout.horizontal.rightleft
+    --}
+--end
+-- }}}
+
+
+-- {{{ Key bindings
+globalkeys = awful.util.table.join(
+	
+	-- Move CDROM in and out
+	awful.key({ modkey, "Control"	}, "KP_Add",		function () awful.util.spawn("eject") end),
+	awful.key({ modkey, "Control"	}, "KP_Subtract",	function () awful.util.spawn("eject -t") end),
+
+	-- dmenu
+	awful.key({ "Control"		}, " ",			function () awful.util.spawn("dmenu_run") end),
+
+	-- Tag movement
+	awful.key({ modkey		}, "Tab",		function () awful.screen.focus_relative(1) end),
+	awful.key({ altkey		}, "Escape",		awful.tag.history.restore),
+	awful.key({ modkey		}, "Left",		awful.tag.viewprev),
+	awful.key({ modkey		}, "Right",		awful.tag.viewnext),
+
+	-- Standard program
+	awful.key({ 			}, "Print",		function () awful.util.spawn("scrot") end),
+	awful.key({ modkey		}, "Print",		function () awful.util.spawn("scrot -s") end),
+	awful.key({ modkey		}, "Return",	function () awful.util.spawn(terminal) end),
+	awful.key({ modkey		}, "l",			function () awful.util.spawn("xscreensaver-command -lock") end),
+
+	-- Awesome
+	awful.key({ modkey, "Shift" 	}, "r",			awesome.restart),
+	awful.key({ modkey, "Shift"	    }, "q",			awesome.quit),
+
+	-- Cycle master/slave clients and switch between them
+	awful.key({ altkey		}, "Right",
+		function ()
+			awful.client.focus.bydirection("right")
+			if client.focus then client.focus:raise() end
+		end),
+
+	awful.key({ altkey 		}, "Left",
+		function ()
+			awful.client.focus.bydirection("left")
+			if client.focus then client.focus:raise() end
+		end),
+
+	awful.key({ altkey		}, "Up",
+		function ()
+			awful.client.focus.bydirection("up")
+			if client.focus then client.focus:raise() end
+		end),
+
+	awful.key({ altkey 		}, "Down",
+		function ()
+			awful.client.focus.bydirection("down")
+			if client.focus then client.focus:raise() end
+		end),
+
+    -- Alt+Tab and Win+N cycles to the next window
+	awful.key({ altkey		}, "Tab",
+		function ()
+			awful.client.focus.byidx(1)
+			if client.focus then client.focus:raise() end
+		end),
+
+	awful.key({ modkey      }, "n",
+		function ()
+			awful.client.focus.byidx(1)
+			if client.focus then client.focus:raise() end
+		end),
+
+    -- Alt+Shift+Tab and Win+H cycles to prev window
+	awful.key({ altkey, "Shift"	}, "Tab",
+		function ()
+			awful.client.focus.byidx(1)
+			if client.focus then
+                client.focus.minimized = false
+                client.focus:raise()
+            end
+		end),
+
+	awful.key({ modkey		}, "h",
+		function ()
+			awful.client.focus.byidx(1)
+			if client.focus then
+                client.focus.minimized = false
+                client.focus:raise()
+            end
+		end),
+
+
+    -- Layout manipulation
+    awful.key({ modkey, "Control" }, " ", function () awful.layout.inc(layouts, 1)	end),
+    awful.key({ modkey, "Control", "Shift" }, " ", function () awful.layout.inc(layouts, -1)	end),
+
+    awful.key({ modkey,           }, "u", awful.client.urgent.jumpto)
+)
+
+clientkeys = awful.util.table.join(
+	awful.key({ modkey  		}, "w",		function (c) c:kill() end),
+
+    -- Move window to next and prev screen
+	awful.key({ modkey, "Shift"  }, "Right",      awful.client.movetoscreen ),
+	awful.key({ modkey, "Shift"  }, "Left",      awful.client.movetoscreen )
+)
+
+-- Compute the maximum number of digit we need, limited to 9
+keynumber = 0
+for s = 1, screen.count() do
+   keynumber = math.min(9, math.max(#tags[s], keynumber));
+end
+
+-- Bind all key numbers to tags.
+-- Be careful: we use keycodes to make it works on any keyboard layout.
+-- This should map on the top row of your keyboard, usually 1 to 9.
+for i = 1, keynumber do
+    globalkeys = awful.util.table.join(globalkeys,
+        awful.key({ modkey }, "#" .. i + 9,
+                  function ()
+                        local screen = mouse.screen
+                        if tags[screen][i] then
+                            awful.tag.viewonly(tags[screen][i])
+                        end
+                  end),
+        awful.key({ modkey, "Control" }, "#" .. i + 9,
+                  function ()
+                      local screen = mouse.screen
+                      if tags[screen][i] then
+                          awful.tag.viewtoggle(tags[screen][i])
+                      end
+                  end),
+        awful.key({ modkey, "Shift" }, "#" .. i + 9,
+                  function ()
+                      if client.focus and tags[client.focus.screen][i] then
+                          awful.client.movetotag(tags[client.focus.screen][i])
+                      end
+                  end),
+        awful.key({ modkey, "Control", "Shift" }, "#" .. i + 9,
+                  function ()
+                      if client.focus and tags[client.focus.screen][i] then
+                          awful.client.toggletag(tags[client.focus.screen][i])
+                      end
+                  end))
+end
+
+clientbuttons = awful.util.table.join(
+    awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
+    awful.button({ modkey }, 1, awful.mouse.client.move),
+    awful.button({ modkey }, 3, awful.mouse.client.resize))
+
+-- Set keys
+root.keys(globalkeys)
+-- }}}
+
+-- {{{ Rules
+awful.rules.rules = {
+    -- All clients will match this rule.
+    { rule = { },
+      properties = { border_width = 0,
+                     focus = true,
+                     keys = clientkeys,
+                     buttons = clientbuttons } }
+}
+-- }}}
+
+-- {{{ Signals
+-- Signal function to execute when a new client appears.
+client.add_signal("manage", function (c, startup)
+    -- Add a titlebar
+    -- awful.titlebar.add(c, { modkey = modkey })
+
+    -- Enable sloppy focus
+    c:add_signal("mouse::enter", function(c)
+        if awful.layout.get(c.screen) ~= awful.layout.suit.magnifier
+            and awful.client.focus.filter(c) then
+            client.focus = c
+        end
+    end)
+
+    if not startup then
+        -- Set the windows at the slave,
+        -- i.e. put it at the end of others instead of setting it master.
+        -- awful.client.setslave(c)
+
+        -- Put windows in a smart way, only if they does not set an initial position.
+        if not c.size_hints.user_position and not c.size_hints.program_position then
+            awful.placement.no_overlap(c)
+            awful.placement.no_offscreen(c)
+        end
+    end
+end)
+
+-- }}}
---
+++ themes/ambient/theme.lua
@@ -0,0 +1,46 @@
+---------------------------
+-- Default awesome theme --
+---------------------------
+
+theme = {}
+
+theme.font          = "sans 8"
+
+theme.bg_normal     = "#222222"
+theme.bg_focus      = "#535d6c"
+theme.bg_urgent     = "#ff0000"
+theme.bg_minimize   = "#444444"
+
+theme.fg_normal     = "#aaaaaa"
+theme.fg_focus      = "#ffffff"
+theme.fg_urgent     = "#ffffff"
+theme.fg_minimize   = "#ffffff"
+
+theme.border_width  = "0"
+
+-- There are other variable sets
+-- overriding the default one when
+-- defined, the sets are:
+-- [taglist|tasklist]_[bg|fg]_[focus|urgent]
+-- titlebar_[bg|fg]_[normal|focus]
+-- tooltip_[font|opacity|fg_color|bg_color|border_width|border_color]
+-- mouse_finder_[color|timeout|animate_timeout|radius|factor]
+-- Example:
+--theme.taglist_bg_focus = "#ff0000"
+
+-- Variables set for theming the menu:
+-- menu_[bg|fg]_[normal|focus]
+-- menu_[border_color|border_width]
+theme.menu_height = "15"
+theme.menu_width  = "100"
+
+-- You can add as many variables as
+-- you wish and access them by using
+-- beautiful.variable in your rc.lua
+--theme.bg_widget = "#cc0000"
+
+-- You can use your own command to set your wallpaper
+theme.wallpaper_cmd = { "awsetbg -a " .. awful.util.getdir("config") .. "/themes/ambient/paper_04.png" }
+
+return theme
+-- vim: filetype=lua:expandtab:shiftwidth=4:tabstop=8:softtabstop=4
